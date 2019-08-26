a0263543754fa88da5960f1d7b166760c6b2a4b5
Eric <eric.audemard@umontreal.ca>
2019-08-26 15:29:48 -0400

    add option to print output on standard output

--- epcy/tools/pred.py
+++ epcy/tools/pred.py
@@ -3,19 +3,50 @@ import time
 import sys
 
 import numpy as np
+import pandas as pd
 
 from multiprocessing import Pool
 
 from .. utils import Classifier as uc
 from .. utils import readers as ur
 
-def run_classifier(args, design, start):
-    c = uc.Classifier(args, design, start)
+def run_classifier(args, design, data, list_ids, start):
+    data_selected = data[start:(start + args.BY)]
+    list_ids_selected = list_ids[start:(start + args.BY)]
+    c = uc.Classifier(args, design, data_selected, list_ids_selected, start)
     c.run()
 
     return(c)
 
-def main_pred(args, argparser):
+def read_design_matrix(args):
+    design = ur.get_design(args)
+    data = pd.io.parsers.read_csv(args.MATRIX, sep="\t", index_col=0)
+    if sum(~design["sample"].isin(data.columns)) > 0:
+        sys.stderr.write("WARNING: Some samples are present in the design, but not in the quantification matrix\n")
+        sys.stderr.write("\t the analysis will be done without these samples:\n")
+        sys.stderr.write(str(design[~design["sample"].isin(data.columns)]) + "\n")
+        design = design[design["sample"].isin(data.columns)]
+
+    data = data.reindex(design["sample"], axis=1)
+
+    if args.CPM:
+        f_norm = 1e6 /  data.iloc[:,1:].sum()
+
+    #data = data[start:(start+args.BY)]
+    data = data[(data.T != 0).any()]
+
+    if args.CPM:
+        data.iloc[:,1:] = data.iloc[:,1:] * f_norm
+
+    list_ids = list(data.index)
+
+    data = data.values
+    if not args.LOG:
+        data = np.log2(data + args.C)
+
+    return(design, data)
+
+def main_pred(args, argparser, list_ids):
 
     if args.ANNO is not None:
         sys.stderr.write(time.strftime('%X') + ": Read annotation\n")
@@ -27,25 +58,24 @@ def main_pred(args, argparser):
         df_anno["Parent"] = df_anno["Parent"].str.replace("transcript:", "")
         df_anno.set_index("ID", inplace=True)
 
-    sys.stderr.write(time.strftime('%X') + ": Read design\n")
-    design = ur.get_design(args)
-
     num_pred = sum(1 for line in open(args.MATRIX, 'r')) - 1
-    # print(num_pred)
 
     sys.stderr.write(time.strftime('%X') + ": Start epcy analysis\n")
     if args.BY == -1:
         args.BY = int(num_pred / args.THREAD) + 1
 
+    sys.stderr.write(time.strftime('%X') + ": Read design and matrix features\n")
+    (design, data, list_ids) = read_design_matrix(args)
+
     all_classifier = []
     if args.THREAD <= 1:
-        tasks = [(args, design, start) for start in np.arange(0, num_pred, args.BY)]
+        tasks = [(args, design, data, list_ids, start) for start in np.arange(0, num_pred, args.BY)]
         for task in tasks:
-            all_classifier.append(run_classifier(task[0], task[1], task[2]))
+            all_classifier.append(run_classifier(task[0], task[1], task[2], task[3], task[4]))
 
     else:
         with Pool(processes=args.THREAD) as p:
-            tasks = [(args, design, start) for start in np.arange(0, num_pred, args.BY)]
+            tasks = [(args, design, data, start) for start in np.arange(0, num_pred, args.BY)]
 
             jobs = [p.apply_async(run_classifier, t) for t in tasks]
             all_classifier = [job.get(None) for job in jobs]
--- epcy/utils/Classifier.py
+++ epcy/utils/Classifier.py
@@ -10,10 +10,12 @@ ne.set_num_threads(1)
 from scipy.stats import mannwhitneyu, ttest_ind
 
 class Classifier:
-    def __init__(self, args, design, start):
+    def __init__(self, args, design, data, list_ids, start):
         self.args = args
         self.start = start
         self.design = design
+        self.data = data
+        self.list_ids = list_ids
 
         self.num_query = len(np.where(design[self.args.SUBGROUP] == 1)[0])
         self.num_ref = len(np.where(design[self.args.SUBGROUP] == 0)[0])
@@ -21,7 +23,7 @@ class Classifier:
         self.done = False
 
     def run(self):
-        self.__load_data()
+        #self.__load_data()
         self.__pred()
 
     @staticmethod
@@ -70,7 +72,7 @@ class Classifier:
 
 
     def __load_data(self):
-        self.data = pd.io.parsers.read_csv(self.args.MATRIX, sep="\t", index_col=0)
+        #self.data = pd.io.parsers.read_csv(self.args.MATRIX, sep="\t", index_col=0)
         if sum(~self.design["sample"].isin(self.data.columns)) > 0:
             sys.stderr.write("WARNING: Some samples are present in the design, but not in the quantification matrix\n")
             sys.stderr.write("\t the analysis will be done without these samples:\n")
@@ -82,7 +84,7 @@ class Classifier:
         if self.args.CPM:
             f_norm = 1e6 /  self.data.iloc[:,1:].sum()
 
-        self.data = self.data[self.start:(self.start+self.args.BY)]
+        #self.data = self.data[self.start:(self.start+self.args.BY)]
         self.data = self.data[(self.data.T != 0).any()]
 
         if self.args.CPM:
