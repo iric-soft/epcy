8043b9c26b577db81e2f38dc693af9cf5a105909
eric audemard <eric.audemard@umontreal.ca>
2020-03-20 10:33:29 -0400

    typo

--- epcy/argparser/get_bandwidth_part.py
+++ epcy/argparser/get_bandwidth_part.py
@@ -3,7 +3,7 @@ def get_argparser_bandwidth_part(parser):
 
     parser.add_argument("--min_bw",
                         dest="MIN_BW",
-                        help="To compute KDE MCC a bandwidth need to " +
+                        help="To compute KDE MCC a bandwidth need to be " +
                              "estimate from data using bw_nrd. To avoid " +
                              "very small bw you can use this parameter to " +
                              "set a minimum (Default:0.1).",
